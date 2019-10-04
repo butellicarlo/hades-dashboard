aa8d00ab0525fa2848e85762669aba1c34492211
Carlo Butelli <c.butelli@nxchange.com>
2019-10-05 01:51:00 +0200

    Added navbar and first implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { directive } from "@babel/types";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>v8s9dfvsd9fva9dsf sd8 sdc98sd</p>
+    </div>
+  );
+};
+
+export default About;
